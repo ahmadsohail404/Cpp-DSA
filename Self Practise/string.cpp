7972fe368c2b2c9a7bc28d5e162ce218d53555a8
Sohail Ahmad <sohailss2412@gmail.com>
2021-09-29 02:01:08 +0530

    modified string literal

--- Self Practise/string.cpp
+++ Self Practise/string.cpp
@@ -2,14 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fun(string &s)
+void fun(const string &s) //const is important here otherwise it won't compile.
 {
     cout << s;
 }
 
 int main()
 {
-    fun("Meow");
+    fun("Meow Meow");
 
     return 0;
 }
