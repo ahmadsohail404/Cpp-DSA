9e4e046a95c8339ad9527fe8d27e60146fc29890
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-08 00:39:30 +0530

    lcm

---
+++ Maths for DSA/06lcmOptimised.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// a*b = gcd(a,b) * lcm(a,b)
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return (b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << lcm(a, b);
+
+    return 0;
+}
