80a052df53c2f920906a2cd3651d4f1952ebf890
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-16 15:58:51 +0530

    pattern

--- Pattern Printing/03triangle.cpp
+++ Pattern Printing/03triangle.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+*/
+
 int main()
 {
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j <= n; j++)
         {
-            if (j >= i)
-                cout << "*"
-                     << " ";
+            if (j >= n + 1 - i)
+                cout << "*";
             else
                 cout << " ";
         }
