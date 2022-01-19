87fbfb9610265335aa9f8d41c3efa209e2646bd1
Sohail Ahmad <sohailss2412@gmail.com>
2022-01-20 03:15:34 +0530

    linkedlist

---
+++ Pattern Printing/08thinPyramid.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+
+
+*/
+
+int main()
+{
+    int n;
+    cout << "Enter the height of the pyramid: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= (2 * n - 1); j++)
+        {
+            if ()
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
