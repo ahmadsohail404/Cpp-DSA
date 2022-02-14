576ff4c88a265c12e94c7d4ecd4182a85023bdc0
Sohail Ahmad <sohailss2412@gmail.com>
2022-02-15 00:10:05 +0530

    arrays

---
+++ Array/removeDup2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDup(int arr[], int n)
+{
+    int res = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] != arr[res - 1])
+        {
+            arr[res++] = arr[i];
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << removeDup(arr, n);
+
+    return 0;
+}
